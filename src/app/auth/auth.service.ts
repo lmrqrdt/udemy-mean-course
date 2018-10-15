81d2dc2c86b8ecbd2b4b121e6cc840909e03f347
LARRY MARQUARDT <lmrqrdt@gmail.com>
2018-10-15 14:10:19 -0600

    mean-course through section 9 authorization

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,6 +10,7 @@ export class AuthService {
   private isAuthenticated = false;
   private token: string;
   private tokenTimer: any;
+  private userId: string;
   private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) {}
@@ -22,6 +23,10 @@ export class AuthService {
     return this.isAuthenticated;
   }
 
+  getUserId() {
+    return this.userId;
+  }
+
   getAuthStatusListener() {
     return this.authStatusListener.asObservable();
   }
@@ -30,14 +35,17 @@ export class AuthService {
     const authData: AuthData = {email: email, password: password};
     this.http.post('http://localhost:3000/api/user/signup', authData)
      .subscribe(response => {
-       console.log(response);
+       this.router.navigate(['/login']);
      });
   }
 
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
     this.http
-    .post<{token: string, expiresIn: number}>('http://localhost:3000/api/user/login', authData)
+    .post<{token: string, expiresIn: number, userId: string}>(
+      'http://localhost:3000/api/user/login',
+      authData
+    )
       .subscribe(response => {
         const token = response.token;
         this.token = token;
@@ -45,11 +53,11 @@ export class AuthService {
           const expiresInDuration = response.expiresIn;
           this.setAuthTimer(expiresInDuration);
           this.isAuthenticated = true;
+          this.userId = response.creator;
           this.authStatusListener.next(true);
           const now = new Date();
           const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
-          console.log(expirationDate);
-          this.saveAuthData(token, expirationDate);
+          this.saveAuthData(token, expirationDate, this.userId);
           this.router.navigate(['/']);
         }
       });
@@ -65,6 +73,7 @@ export class AuthService {
     if (expiresIn > 0) {
       this.token = authInformation.token;
       this.isAuthenticated = true;
+      this.userId = authInformation.userId;
       this.setAuthTimer(expiresIn / 1000);
       this.authStatusListener.next(true);
     }
@@ -74,37 +83,41 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    this.userId = null;
     clearTimeout(this.tokenTimer);
     this.clearAuthData();
     this.router.navigate(['/']);
   }
 
   private setAuthTimer(duration: number) {
-    console.log('Setting timer: ' + duration);
     this.tokenTimer = setTimeout(() => {
       this.logout();
     }, duration * 1000);
   }
 
-  private saveAuthData(token: string, expirationDate: Date) {
+  private saveAuthData(token: string, expirationDate: Date, userId: string) {
     localStorage.setItem('token', token);
     localStorage.setItem('expiration', expirationDate.toISOString());
+    localStorage.setItem('userId', userId);
   }
 
   private clearAuthData() {
     localStorage.removeItem('token');
     localStorage.removeItem('expiration');
+    localStorage.removeItem('userId');
   }
 
   private getAuthData() {
     const token = localStorage.getItem('token');
     const expirationDate = localStorage.getItem('expiration');
+    const userId = localStorage.getItem('userId');
     if (!token || !expirationDate) {
       return;
     }
     return {
       token: token,
-      expirationDate: new Date(expirationDate)
+      expirationDate: new Date(expirationDate),
+      userId: userId
     };
   }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,23 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
 import { AuthService } from '../auth.service';
+import { Subscription } from '../../../../node_modules/rxjs';
 
 @Component({
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css']
 })
 
-export class SignupComponent {
-  isLoading = false;
 
-  constructor(public authService: AuthService) {}
+export class SignupComponent implements OnInit, OnDestroy {
+    isLoading = false;
+    private authStatusSub: Subscription;
 
-  onSignup(form: NgForm) {
-    if (form.invalid) {
-      return;
+    constructor(public authService: AuthService) {}
+
+    ngOnInit() {
+      this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
+        authStatus => {
+          this.isLoading = false;
+        }
+      );
     }
-    this.isLoading = true;
-    this.authService.createUser(form.value.email, form.value.password);
+
+    onSignup(form: NgForm) {
+      if (form.invalid) {
+        return;
+      }
+      this.isLoading = true;
+      this.authService.createUser(form.value.email, form.value.password);
+    }
+
+    ngOnDestroy() {
+      this.authStatusSub.unsubscribe();
+    }
   }
-}
